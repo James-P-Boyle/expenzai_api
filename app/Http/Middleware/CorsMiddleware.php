39e07664a7d54a0894b4e46053127f4196540687
James Boyle <98388371+James-P-Boyle@users.noreply.github.com>
2025-07-17 12:14:02 +0200

    Fix

--- app/Http/Middleware/CorsMiddleware.php
+++ app/Http/Middleware/CorsMiddleware.php
@@ -1,5 +1,4 @@
 <?php
-
 namespace App\Http\Middleware;
 
 use Closure;
@@ -10,21 +9,28 @@ class CorsMiddleware
 {
     public function handle(Request $request, Closure $next): Response
     {
-        $frontendUrl = env('FRONTEND_URL', 'http://localhost:3000');
+        $allowedOrigins = [
+            'https://www.expenzai.app',
+            'https://expenzai.app',
+            'http://localhost:3000' 
+        ];
         
+        $origin = $request->headers->get('Origin');
+        $allowedOrigin = in_array($origin, $allowedOrigins) ? $origin : $allowedOrigins[0];
+
         // Handle preflight OPTIONS requests
         if ($request->isMethod('OPTIONS')) {
             return response('', 200)
-                ->header('Access-Control-Allow-Origin', $frontendUrl)
+                ->header('Access-Control-Allow-Origin', $allowedOrigin)
                 ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
                 ->header('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-Requested-With, Accept')
                 ->header('Access-Control-Allow-Credentials', 'true');
         }
-    
+
         $response = $next($request);
-    
+
         return $response
-            ->header('Access-Control-Allow-Origin', $frontendUrl)
+            ->header('Access-Control-Allow-Origin', $allowedOrigin)
             ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
             ->header('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-Requested-With, Accept')
             ->header('Access-Control-Allow-Credentials', 'true');
