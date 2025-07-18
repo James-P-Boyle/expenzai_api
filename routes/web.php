ebdfd83f89184bdf82c76a00593b4cc30dbb4d40
James Boyle <98388371+James-P-Boyle@users.noreply.github.com>
2025-07-18 19:00:07 +0200

    Add debug route

--- routes/web.php
+++ routes/web.php
@@ -9,3 +9,12 @@ Route::get('/', function () {
         'timestamp' => now()
     ]);
 });
+
+Route::get('/php-limits', function () {
+    return response()->json([
+        'upload_max_filesize' => ini_get('upload_max_filesize'),
+        'post_max_size' => ini_get('post_max_size'),
+        'max_execution_time' => ini_get('max_execution_time'),
+        'memory_limit' => ini_get('memory_limit'),
+    ]);
+});
